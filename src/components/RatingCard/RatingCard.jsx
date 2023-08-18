b25c39af0024049f8b7709cc2501ac574e150517
mdyasin111 <mdyasin7636@gmail.com>
2023-08-18 20:12:27 +0600

    ratting added

--- src/components/RatingCard/RatingCard.jsx
+++ src/components/RatingCard/RatingCard.jsx
@@ -1,13 +1,29 @@
-import { H1, RatingCardWrapper, RatingIconBox } from "./ratingCardStyle";
+import { H1, P, RatingCardWrapper, RatingIconBox, StarButton, SubmitButton } from "./ratingCardStyle";
 import Star from "../../assets/star.svg";
+import { useState } from "react";
 
 const RatingCard = () => {
+  
+    const rating = [1, 2, 3, 4, 5];
+
+    const [selectedRating, setSelectedRating] = useState(0);
+
+    console.log(selectedRating);
+
   return (
     <RatingCardWrapper>
       <RatingIconBox>
-        <img src={Star} alt="" />
+        <img src={Star} alt="Star Icon" />
       </RatingIconBox>
       <H1>How did we do?</H1>
+      <P>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering.
+      </P>
+      {
+        rating.map((item, i) => (<StarButton as='button' key={i} onClick={() => setSelectedRating(item)}>{item}</StarButton>))
+      }
+      <SubmitButton>Submit</SubmitButton>
     </RatingCardWrapper>
   );
 };
--- src/components/RatingCard/ratingCardStyle.js
+++ src/components/RatingCard/ratingCardStyle.js
@@ -8,7 +8,6 @@ min-width: 450px;
 /* background: white; */
 padding: 2.2rem;
 background-color: hsl(213, 19%, 18%);
-
 `;
 
 export const RatingIconBox = styled.div`
@@ -19,13 +18,57 @@ background-color: hsl(210, 16%, 22%);
 display: flex;
 justify-content: center;
 align-items: center;
-
 `
 
 export const H1 = styled.h1`
-color: #ffffff;
+color: #fff;
 font-size: 1.8rem;
 letter-spacing: 1.5px;
 margin: 2rem 0 1rem 0;
 text-align: left;
+`
+
+export const P = styled.p`
+color: hsl(217, 12%, 63%);
+font-size: 1.1rem;
+margin: 1rem 0 2rem 0;
+text-align: left;
+`
+
+export const StarButton = styled(RatingIconBox)`
+display: inline-block;
+border: none;
+font-size: 1rem;
+cursor: pointer;
+color: #fff;
+font-weight: 700;
+
+&:not(:last-child) {
+    margin-right: 1rem;
+}
+
+&:hover {
+    background-color: hsl(25, 97%, 53%);
+    transition: all 0.2s;
+}
+`
+
+export const SubmitButton = styled.button`
+border-radius: 25px;
+width: 100%;
+background-color: hsl(25, 97%, 53%);
+color: #fff;
+font-size: 1rem;
+font-weight: 700;
+border: none;
+padding: 0.8rem;
+margin-top: 2rem;
+cursor: pointer;
+
+&:hover {
+    background-color: #fff;
+    color: hsl(25, 97%, 53%);
+    transition: all 0.2s;
+}
+
 `
